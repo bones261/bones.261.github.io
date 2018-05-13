3ecb2a3728d91d20ad65076bab28585d46a0f955
bones261 <bones261@comcast.net>
2018-05-12 19:07:56 -0600

    Add files via upload

--- js/cheat.js
+++ js/cheat.js
@@ -9,29 +9,6 @@
     e('#detector').scope().dc.click();
   }
 
-  function u() {
-    var rcScope = e('#researchContent').scope().rc;
-    rcScope.research.forEach(function(r) {
-      if (rcScope.isAvailable(r)) {
-        rcScope.doResearch(r);
-      }
-    });
-
-    var hrScope = e('#hrContent').scope().hrc;
-    hrScope.workers.forEach(function(w) {
-      if (hrScope.isAvailable(w)) {
-        hrScope.hire(w);
-      }
-    });
-
-    var ucScope = e('#upgradesContent').scope().uc;
-    ucScope.upgrades.forEach(function(u) {
-      if (ucScope.isAvailable(u)) {
-        ucScope.upgrade(u);
-      }
-    });
-  }
-
   setInterval(c, 100);
-  setInterval(u, 10);
+
 })(angular);
